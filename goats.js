53027366f8a2eb7806dda7a1b20ccaf1ca811971
Genadi Tsolov <genadi.tsolov@gmail.com>
2022-07-07 12:32:42 +0300

    program is working, needed fix on search function

--- goats.js
+++ goats.js
@@ -10,28 +10,36 @@ const raft = (numberGoats, numberCourses, goatsSizes = []) => {
 
   while (!isFinished) {
     let goatsLeft = goatsSizes;
-    let currentWeight = 0;
-    const heaviestGoatWeight = getHeaviestGoat(goatsLeft);
-    currentWeight += heaviestGoatWeight;
-    goatsLeft = removeGoat(goatsLeft, heaviestGoatWeight);
 
-    const secondheaviestGoatWeight = getHeaviestGoat(goatsLeft);
-    goatsLeft = removeGoat(goatsLeft, secondheaviestGoatWeight);
+    let coursesGoats = [];
+    for (let i = 0; i < numberCourses; i++) {
+      const heaviestGoatWeight = getHeaviestGoat(goatsLeft);
+      goatsLeft = removeGoat(goatsLeft, heaviestGoatWeight);
+      coursesGoats = [...coursesGoats, [heaviestGoatWeight]];
+    }
+    // console.log(coursesGoats);
 
-    while (minCapacity > currentWeight) {
-      const closest = findClosest(goatsLeft, minCapacity - currentWeight);
-      console.log(closest);
-      if (closest + currentWeight <= minCapacity) {
-        currentWeight += closest;
-        goatsLeft = removeGoat(goatsLeft, closest);
+    for (let i = 0; i < coursesGoats.length; i++) {
+      let weight = coursesGoats[i][0];
+      let isFull = false;
+      while (!isFull) {
+        const closest = findClosest(goatsLeft, minCapacity - weight);
+        // console.log(closest);
+        if (closest > 0 && minCapacity >= weight + closest) {
+          coursesGoats[i].push(closest);
+          goatsLeft = removeGoat(goatsLeft, closest);
+          weight += closest;
+        } else {
+          isFull = true;
+        }
       }
-      // if(minCapacity >= (currentWeight + goatsLeft[j])){
-      //     currentWeight += goatsLeft[j];
-      //     goatsLeft = removeGoat(goatsLeft, goatsLeft[j])
-      // }
+      // console.log(coursesGoats[i]);
+      // console.log(goatsLeft);
     }
 
-    if (calculateTotalGoatWeight(goatsLeft) > minCapacity) {
+    console.log(coursesGoats);
+
+    if (calculateTotalGoatWeight(goatsLeft) !== 0) {
       minCapacity++;
     } else {
       isFinished = true;
@@ -68,9 +76,21 @@ const removeGoat = (goats = [], weight) => {
 };
 
 const findClosest = (array = [], value) => {
-  return array.sort((a, b) => Math.abs(value - a) - Math.abs(value - b))[0];
+  if (array.length === 0) return 0;
+  const sorted = array.sort(
+    (a, b) => Math.abs(value - a) - Math.abs(value - b)
+  );
+  return sorted.find((elm) => elm <= value);
 };
 
 console.log(raft(6, 2, [26, 7, 10, 30, 5, 4]));
-// console.log(raft(6,2,[4,8,15,16,23,42]))
-// console.log(raft(15,3,[666,42,7,13,400,511,600,200,202,111,313,94,280,72,42]))
+console.log("**********************************");
+console.log(raft(6, 2, [4, 8, 15, 16, 23, 42]));
+console.log("**********************************");
+console.log(
+  raft(
+    15,
+    3,
+    [666, 42, 7, 13, 400, 511, 600, 200, 202, 111, 313, 94, 280, 72, 42]
+  )
+);
